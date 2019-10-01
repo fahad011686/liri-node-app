6c3d40d7dd755d460d39e99afa296d1d6e3100d2
Fahad <fahad011686@gmail.com>
2019-10-01 17:22:58 -0500

    Spotify API is working.

--- liri.js
+++ liri.js
@@ -1,14 +1,30 @@
 require('dotenv').config();
 var inquirer = require("inquirer");
 var keys = require("./keys.js");
-console.log(keys);
+// console.log(keys);
 
 // spotify this
 var Spotify = require('node-spotify-api');
+// var spotify = new Spotify(keys.spotify);
+// above and below both seem to work now
 var spotify = new Spotify({
-    id: spotify.id,
-    secret: spotify.secret
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
 });
+
+var spotifySearch = "The Sign";
+
+var spotifyThis = function {
+spotify.search({ type: 'track', query: spotifySearch }, function (err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+    console.log(data.tracks.items[0].name);
+    console.log(data.tracks.items[0].album.artists[0].name);
+    console.log("Album: " + data.tracks.items[0].album.name + " (" + (data.tracks.items[0].album.release_date.split('-'))[0] + ")");
+
+});
+}
 
 //movie-this
 var axios = require("axios");
@@ -26,7 +42,7 @@ axios.get("http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&apikey=tr
             console.log("---------------Status---------------");
             console.log(error.response.headers);
         } else if (error.request) {
-            
+
             console.log(error.request);
         } else {
             console.log("Error", error.message);
